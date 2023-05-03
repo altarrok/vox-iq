9203456690c23b780c3a7101af48f15cf2bb21dc
Altay Batuhan <altaybatuhanmail@gmail.com>
2023-05-03 08:22:15 -0700

    styling chat bubbles

--- apps/vocal-ui/src/App.tsx
+++ apps/vocal-ui/src/App.tsx
@@ -1,7 +1,7 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { httpBatchLink } from '@trpc/client';
 import { useState } from 'react';
-import { StyleSheet, View } from 'react-native';
+import { Platform, SafeAreaView, StatusBar, StyleSheet, View } from 'react-native';
 import { trpc } from './utils/trpc';
 import { ChatContextProvider } from './components/chat/ChatContext';
 import { ChatWidget } from './components/chat/ChatWidget';
@@ -19,15 +19,19 @@ export default function App() {
     }),
   );
 
+  const statusBarHeight = Platform.OS === 'android' ? StatusBar.currentHeight : 0;
 
   return (
     <trpc.Provider client={trpcClient} queryClient={queryClient}>
       <QueryClientProvider client={queryClient}>
         <ChatContextProvider>
-          <View style={styles.container}>
-            <ChatWidget />
-            <ChatButton />
-          </View>
+          <SafeAreaView style={[styles.container, { paddingTop: statusBarHeight }]}>
+            <View style={styles.innerContainer}>
+              <StatusBar barStyle="light-content" />
+              <ChatWidget />
+              <ChatButton />
+            </View>
+          </SafeAreaView>
         </ChatContextProvider>
       </QueryClientProvider>
     </trpc.Provider >
@@ -37,9 +41,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#2e2e2e',
+  },
+  innerContainer: {
+    flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+    marginTop: 16,
   },
 });
-
--- apps/vocal-ui/src/components/chat/ChatBox.tsx
+++ apps/vocal-ui/src/components/chat/ChatBox.tsx
@@ -1,5 +1,6 @@
 import { useRef, useState } from "react";
-import { ScrollView, StyleSheet, Text } from "react-native";
+import { ScrollView, StyleSheet, Text, View } from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
 import { TMessage } from "./ChatContext";
 
 export const ChatBox: React.FC<{ messages: TMessage[] }> = ({ messages }) => {
@@ -17,16 +18,80 @@ export const ChatBox: React.FC<{ messages: TMessage[] }> = ({ messages }) => {
                 setIsAtBottom(isUserAtBottom);
             }}
             scrollEventThrottle={16}
+            showsVerticalScrollIndicator={false}
         >
-            {messages.map((message, i) => (message.role !== "system") ? <Text key={i}>{message.role}: {message.content}</Text> : <></>)}
+            {messages.map((message, i) => (
+                message.role !== "system" ? (
+                    <View key={i} style={message.role === "user" ? style.userBubbleContainer : style.aiBubbleContainer}>
+                        {message.role === "assistant" && <Text style={style.chatGPTLabel}>ChatGPT</Text>}
+                        {message.role === "user" ? (
+                            <View style={style.userBubble}>
+                                <Text style={style.bubbleText}>
+                                    {message.content}
+                                </Text>
+                            </View>
+                        ) : (
+                            <LinearGradient
+                                colors={["rgba(131,58,180,1)", "rgba(0,121,191,1)", "rgba(172,0,0,1)"]}
+                                start={{ x: 0, y: 0 }}
+                                end={{ x: 1, y: 0 }}
+                                style={[style.aiBubble, style.linearGradient]}
+                            >
+                                <Text style={style.bubbleText}>
+                                    {message.content}
+                                </Text>
+                            </LinearGradient>
+                        )}
+                    </View>
+                ) : null
+            ))}
         </ScrollView>
     );
 }
 
 const style = StyleSheet.create({
     scrollView: {
-        backgroundColor: 'pink',
         marginBottom: 15,
         width: '100%',
+    },
+    userBubbleContainer: {
+        alignSelf: 'flex-end',
+        marginBottom: 8,
+        marginRight: 10,
+        maxWidth: '80%',
+    },
+    userBubble: {
+        backgroundColor: '#007AFF',
+        borderRadius: 18,
+        paddingHorizontal: 16,
+        paddingVertical: 10,
+    },
+    aiBubbleContainer: {
+        alignSelf: 'flex-start',
+        marginBottom: 8,
+        marginLeft: 10,
+        maxWidth: '80%',
+    },
+    aiBubble: {
+        borderRadius: 18,
+        paddingHorizontal: 16,
+        paddingVertical: 10,
+    },
+    linearGradient: {
+        borderRadius: 18,
+        paddingHorizontal: 16,
+        paddingVertical: 10,
+    },
+    bubbleText: {
+        color: 'white',
+        textAlign: 'left',
+    },
+    chatGPTLabel: {
+        alignSelf: 'flex-start',
+        color: '#787878',
+        fontSize: 12,
+        fontStyle: 'italic',
+        marginBottom: 2,
+        marginLeft: 10,
     },
 });
