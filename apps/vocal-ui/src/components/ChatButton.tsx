cf6069e1a3511a6c8f9cd8a2000446e96be8196d
Kutluay Cakadur <kcakadur@collectivei.com>
2023-05-03 22:27:36 +0300

    convert recording to base64 string

--- apps/vocal-ui/src/components/ChatButton.tsx
+++ apps/vocal-ui/src/components/ChatButton.tsx
@@ -4,9 +4,6 @@ import { useChatContext } from "./chat/ChatContext";
 import FontAwesome from "@expo/vector-icons/FontAwesome";
 import { useEffect, useState } from "react";
 import { Audio } from "expo-av";
-import * as FileSystem from "expo-file-system";
-
-const testAudioBase64 = "lol";
 
 export const ChatButton: React.FC = () => {
   const [recording, setRecording] = useState(null);
@@ -71,7 +68,39 @@ export const ChatButton: React.FC = () => {
     try {
       if (recordingStatus === "recording") {
         await recording.stopAndUnloadAsync();
-        setRecordingStatus("stopped");
+        
+
+        const blobToBase64 = (blob:any) => {
+          const reader = new FileReader();
+          reader.readAsDataURL(blob);
+          return new Promise((resolve) => {
+            reader.onloadend = () => {
+              resolve(reader.result);
+            };
+          });
+        };
+
+        // Fetch audio binary blob data
+
+        const audioURI = recording.getURI();
+        const blob = await new Promise((resolve, reject) => {
+          const xhr = new XMLHttpRequest();
+          xhr.onload = function () {
+            resolve(xhr.response);
+          };
+          xhr.onerror = function (e) {
+            reject(new TypeError("Network request failed"));
+          };
+          xhr.responseType = "blob";
+          xhr.open("GET", audioURI, true);
+          xhr.send(null);
+        });
+
+        const audioBase64Data = await blobToBase64(blob) as string;
+        const audioBase64 = audioBase64Data.split(",")[1];
+        transcribeMutation.mutate({recording: audioBase64, format: "m4a"})
+        console.log(audioBase64);
+        setRecordingStatus("idle");
         setRecording(null);
       }
     } catch (err) {
@@ -81,7 +110,7 @@ export const ChatButton: React.FC = () => {
 
   return (
     <FontAwesome.Button
-      name={recording ? "microphone-slash" : "microphone"}
+      name={recordingStatus === "idle" ? "microphone" : "microphone-slash"}
       size={64}
       onPress={() =>
         recordingStatus === "idle" ? startRecording() : stopRecording()
